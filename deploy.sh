367f2d99fe07526f20ec7c933a236f64c7ad5142
welle-ai <fall.momar_aly_dom@uqam.ca>
2024-08-06 11:04:34 -0400

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 export servicename=document-ai-adapter
 export PROJECT_ID=$(gcloud config get-value project)
-export REGION=europe-west1
-export ENV=GCP_DOCAI_REGION=eu,GCP_DOCAI_PROCESSOR_ID=eade9760f6f088f
+export REGION=us
+export ENV=GCP_DOCAI_REGION=us,GCP_DOCAI_PROCESSOR_ID=a163f18673d69d3
 
 # docker build -t local/$servicename .
-# docker tag local/$servicename eu.gcr.io/$PROJECT_ID/$servicename
-# docker push eu.gcr.io/$PROJECT_ID/$servicename
+# docker tag local/$servicename us.gcr.io/$PROJECT_ID/$servicename
+# docker push us.gcr.io/$PROJECT_ID/$servicename
 
-gcloud builds submit --tag "eu.gcr.io/$PROJECT_ID/$servicename"
+gcloud builds submit --tag "us.gcr.io/$PROJECT_ID/$servicename"
 
-gcloud run deploy $servicename --image eu.gcr.io/$PROJECT_ID/$servicename --platform managed --project $PROJECT_ID --region $REGION --update-env-vars $ENV --allow-unauthenticated
+gcloud run deploy $servicename --image us.gcr.io/$PROJECT_ID/$servicename --platform managed --project $PROJECT_ID --region $REGION --update-env-vars $ENV --allow-unauthenticated
